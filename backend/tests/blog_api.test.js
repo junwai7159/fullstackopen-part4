78482f28d59b14f77abd8a2497c558d59e274288
junwai7159 <junwai7159@gmail.com>
2024-07-18 19:35:21 +0800

    Added user_api.test.js

--- backend/tests/blog_api.test.js
+++ backend/tests/blog_api.test.js
@@ -41,92 +41,112 @@ test('identifying field is named id', async () => {
   expect(response.body[0].id).toBeDefined()
 })
 
-test('a valid blog can be added', async () => {
+test('a valid blog can be added ', async () => {
   const initialResponse = await api.get('/api/blogs')
 
   const newBlog = {
-    title: "Full Stack",
-    author: "John Doe",
-    url: "https://full-stack.com",
-    likes: 1
+      title: "Full Stack",
+      author: "StackMaster",
+      url: "https://stack.com/",
+      likes: 1
   }
 
   await api
-    .post('/api/blogs')
-    .send(newBlog)
-  
+      .post('/api/blogs')
+      .send(newBlog)
+      .set('Authorization', `bearer ${token}`)
+
   const response = await api.get('/api/blogs')
   expect(response.body).toHaveLength(initialResponse.body.length + 1)
 })
 
-test('if blog is added with no likes, zero will be assumed', async () => {
+test('if blog is added with no votes zero will be assumed', async () => {
   const newBlog = {
-    title: "Full Stack",
-    author: "John Doe",
-    url: "https://full-stack.com"
+      title: "Half Stack",
+      author: "StackDiscipline",
+      url: "https://halfstack.com/"
   }
 
   const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
+      .post('/api/blogs')
+      .send(newBlog)
+      .set('Authorization', `bearer ${token}`)
 
   expect(response.body.likes).toBeDefined()
 })
 
 test('if blog is added with no url or title it will not be added', async () => {
   const newBlog = {
-    author: "John Doe",
-    url: "https://full-stack.com",
-    likes: 1
+      author: null,
+      url: "https://stack.com/",
+      likes: 1
   }
 
   const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
+      .post('/api/blogs')
+      .send(newBlog)
+      .set('Authorization', `bearer ${token}`)
 
   expect(response.status).toBe(400)
 })
 
 test('a blog may be removed by issuing http delete request', async () => {
   const newBlog = {
-    title: "Full Stack",
-    author: "StackMaster",
-    url: "https://stack.com/",
-    likes: 1
+      title: "Full Stack",
+      author: "StackMaster",
+      url: "https://stack.com/",
+      likes: 1
   }
 
   const result = await api
-    .post('/api/blogs')
-    .send(newBlog)
-  
+      .post('/api/blogs')
+      .send(newBlog)
+      .set('Authorization', `bearer ${token}`)
+
   const response = await api.get(`/api/blogs/${result.body.id}`)
   const deleteBlog = await api
-    .delete('/api/blogs/${result.body.id}')
-  
+      .delete(`/api/blogs/${result.body.id}`)
+      .set('Authorization', `bearer ${token}`)
   expect(deleteBlog.status).toBe(204)
 })
 
 test('a blog may be edited by issuing http put request', async () => {
   const newBlog = {
-    title: "Full Stack",
-    author: "StackMaster",
-    url: "https://stack.com/",
-    likes: 1
+      title: "Full Stack",
+      author: "StackMaster",
+      url: "https://stack.com/",
+      likes: 1
   }
 
   const result = await api
-    .post('/api/blogs')
-    .send(newBlog)
-  
+      .post('/api/blogs')
+      .send(newBlog)
+      .set('Authorization', `bearer ${token}`)
+
   newBlog.likes += 1
 
   await api
-    .put(`/api/blogs/${result.body.id}`)
-    .send(newBlog)
-  
+      .put(`/api/blogs/${result.body.id}`)
+      .send(newBlog)
+      .set('Authorization', `bearer ${token}`)
   const newResult = await api.get(`/api/blogs/${result.body.id}`)
-
   expect(newResult.body.likes).toBe(newBlog.likes)
+})
+
+test('cannot add blogs without a valid token', async () => {
+  const newBlog = {
+      title: "Full Stack",
+      author: "StackMaster",
+      url: "https://stack.com/",
+      likes: 1
+  }
+
+  const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .set('Authorization', `bearer badly forged token`)
+
+  expect(response.status).toBe(401)
 })
 
 after(async () => {
